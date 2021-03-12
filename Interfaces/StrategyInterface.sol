38b139fb70d733ddb116cc9d960d779fca774004
svrapidinnovation <69822601+svrapidinnovation@users.noreply.github.com>
2021-03-12 18:33:51 +0530

    Update StrategyInterface.sol

--- Interfaces/StrategyInterface.sol
+++ Interfaces/StrategyInterface.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.6.0;
 
 
 interface rStrategy {
+
     function deposit(uint256[3] calldata) external;
     function withdraw(uint256[3] calldata) external;
     function withdrawAll()  external returns(uint256[3] memory);
+    
 }
